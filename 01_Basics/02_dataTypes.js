928956911d433cb479d0e6c18de7853cb5d57ffe
oscar <oscar@claimstrak.co.uk>
2023-10-02 16:14:44 +0545

    lesson2:Data types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+'use strict'; // Treat all JS code as newer version
+
+// alert(3 + 3);  //we're using node js not browser
+
+console.log('Hitesh');
+
+let name = 'Hitesh';
+let age = 3;
+let isLoggedIn = true;
+let address = null;
+let state; //undefined: VARIABLE is defined but VALUE isnt assigned
+
+/*--------Primitive Data Types---------------- */
+//number => 2 to power 53
+//biginit
+//string => "----"
+//boolean => true/false
+//null => standalone value(empty value) / datatype object
+//undefined => VARIABLE is defined but VALUE isnt assigned / datatype undefined
+//symbol => unique
+
+//object
+
+console.log(typeof undefined);
