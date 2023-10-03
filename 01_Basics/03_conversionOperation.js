a5ab98673c779dfca6a918dcfc53250a39886616
oscar <oscar@claimstrak.co.uk>
2023-10-03 13:58:31 +0545

    lesson3: Conversion of Data types

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,9 +1,32 @@
-/* Process of converting one datatype to another datatype.
-  Example : converting  data to number .
-  There are 2 type conversions in JS:
-    1)Implicit conversion- automatic type conversion
-    2)Explicit conversion - manual type conversion
-*/
+/*---------------NOTES---------------- */
+/* 
+  ====>  Process of converting one datatype to another datatype.
+          Example : converting  data to number .
+            There are 2 type conversions in JS:
+              1)Implicit conversion- automatic type conversion
+              2)Explicit conversion - manual type conversion
 
-let score = 33;
-console.log(typeof score);
+  ====>  Number() cases: "33" (converts to)=> 33
+                            "33abc" (converts to)=> NaN
+                            true (converts to)=> 1; false (converts to)=> 0
+
+  ====>  Boolean() cases: 0 => false , 1 =>true , other number => true
+                          "" => false ,  "Hitesh" => ture
+*/
+// ---------------------------------------------------------------------------
+
+let score = 'Hitesh'; // null/undefined/boolean
+//console.log(typeof score);
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = 'Hitesh';
+let booleanISLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanISLoggedIn);
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
