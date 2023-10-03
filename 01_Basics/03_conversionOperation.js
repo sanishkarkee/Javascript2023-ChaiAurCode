24a25d4e3f9ea7c2e214a75e4b986a07de0e70d9
oscar <oscar@claimstrak.co.uk>
2023-10-03 17:26:09 +0545

    lesson4: Why string to number conversion is confusing

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-/*---------------NOTES---------------- */
+/*------ VIDEO3: ---------NOTES---------------- */
 /* 
   ====>  Process of converting one datatype to another datatype.
           Example : converting  data to number .
@@ -13,7 +13,7 @@
   ====>  Boolean() cases: 0 => false , 1 =>true , other number => true
                           "" => false ,  "Hitesh" => ture
 */
-// ---------------------------------------------------------------------------
+// -------VIDEO3:--------------------------------------------------------
 
 let score = 'Hitesh'; // null/undefined/boolean
 //console.log(typeof score);
@@ -28,5 +28,39 @@ let booleanISLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/*------------------VIDEO 4: Operations---------------------------- */
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = 'hello';
+let str2 = ' hitesh';
+
+let str3 = str1 + str2;
+// console.log(str3);    Op=>hello hitesh
+
+// console.log('1' + 2); Op=> 12(string)
+// console.log(1 + '2'); Op=> 12(string)
+// console.log('1' + 2 + 3);   Op => 123(string)
+// console.log(1 + 2 + ' 3');   left bata herdai aaucha ani number haru add garera string sanga concatenate garcha / Op => 33(string)
+
+// console.log(+true);
+// console.log(+'');
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++; //gameCounter = gameCounter +1;
+console.log(gameCounter);
