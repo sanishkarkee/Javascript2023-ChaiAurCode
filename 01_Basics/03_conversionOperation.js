489ce1afbf988254f22dc8126c51fcfdcb1df350
oscar <oscar@claimstrak.co.uk>
2023-10-11 12:22:17 +0545

    04: Comparision

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -15,26 +15,26 @@
 */
 // -------VIDEO3:--------------------------------------------------------
 
-let score = 'Hitesh'; // null/undefined/boolean
+let score = 'Hitesh' // null/undefined/boolean
 //console.log(typeof score);
 
-let valueInNumber = Number(score);
+let valueInNumber = Number(score)
 //console.log(typeof valueInNumber);
 //console.log(valueInNumber);
 
-let isLoggedIn = 'Hitesh';
-let booleanISLoggedIn = Boolean(isLoggedIn);
+let isLoggedIn = 'Hitesh'
+let booleanISLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanISLoggedIn);
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
+let someNumber = 33
+let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
 /*------------------VIDEO 4: Operations---------------------------- */
 
-let value = 3;
-let negValue = -value;
+let value = 3
+let negValue = -value
 // console.log(negValue);
 
 // console.log(2 + 2);
@@ -44,10 +44,10 @@ let negValue = -value;
 // console.log(2 / 3);
 // console.log(2 % 3);
 
-let str1 = 'hello';
-let str2 = ' hitesh';
+let str1 = 'hello'
+let str2 = ' hitesh'
 
-let str3 = str1 + str2;
+let str3 = str1 + str2
 // console.log(str3);    Op=>hello hitesh
 
 // console.log('1' + 2); Op=> 12(string)
@@ -58,9 +58,9 @@ let str3 = str1 + str2;
 // console.log(+true);
 // console.log(+'');
 
-let num1, num2, num3;
-num1 = num2 = num3 = 2 + 3;
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 3
 
-let gameCounter = 100;
-gameCounter++; //gameCounter = gameCounter +1;
-console.log(gameCounter);
+let gameCounter = 100
+gameCounter++ //gameCounter = gameCounter +1;
+console.log(gameCounter)
