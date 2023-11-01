653827b5845ecbfbb67c707b44468d8f8af262d3
oscar <oscar@claimstrak.co.uk>
2023-11-01 12:32:07 +0545

    04: Comparision

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,17 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log('2' > 1)
+// console.log('02' > 1)
+
+// --- diff betwn comparison(<,>) and equality(=) is that , comparison convert null to a number,treating it as a 0
+// console.log(null > 0) //false
+// console.log(null == 0) //false
+// console.log(null >= 0) //true
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
